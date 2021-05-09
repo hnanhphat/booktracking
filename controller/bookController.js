c7814f4d2d0d739db6644ea3e26624507d6225bd
Anh Phat <hnanhphat@Anhs-iMac.local>
2021-05-09 12:09:02 +0700

    update code

--- controller/bookController.js
+++ controller/bookController.js
@@ -4,15 +4,45 @@ const bookController = {};
 
 bookController.getAllData = async (req, res, next) => {
   try {
-    const book = await Book.find()
-      .populate("author", "-_id -__v")
-      .populate("genres", "-_id -__v")
+    // 1. Read the query information
+    let { page, limit, sortBy, ...filter } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+
+    // 2. Get total blog number
+    const totalBooks = await Book.countDocuments({
+      ...filter,
+      // isDelete: false,
+    });
+
+    // 3. Calculate total page number
+    const totalPages = Math.ceil(totalBooks / limit);
+
+    // 4. Calculate how many data you will skip (offset)
+    const offset = (page - 1) * limit;
+
+    // 5. Get blog based on query info
+    const books = await Book.find(filter)
+      .skip(offset)
+      .limit(limit)
+      .populate("author")
       .populate("owner");
 
+    // 6. Send bookData + totalPages info
     res.status(200).json({
       status: "Success",
-      data: book,
+      data: { books, totalPages },
     });
+
+    // const book = await Book.find()
+    //   .populate("author", "-_id -__v")
+    //   .populate("genres", "-_id -__v")
+    //   .populate("owner");
+
+    // res.status(200).json({
+    //   status: "Success",
+    //   data: book,
+    // });
   } catch (error) {
     res.status(400).json({
       status: "Fail",
